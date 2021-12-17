144a0c8f9d2c6fa6a15f8c0f735f494ec74caee5
SilvioChito <93883116+SilvioChito@users.noreply.github.com>
2021-12-17 10:08:14 +0100

    Add files via upload
    Sorting a file using Bubble-sort and multiple processes

---
+++ sorting_with_more_processes.c
@@ -0,0 +1,81 @@
+/*
+ *  Concurrent Version A:
+ *  - Father generate child and wait for him
+ *  - Child compare-exchange and quit
+ *  Comparison (and exchange) are done before the
+ *  new fork on the way down.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <time.h>
+
+void child(int fd, int j);
+
+int main(int argc, char* argv[]){
+  int fd, n, x;
+  int i,j; //contatori
+
+  fd = creat (argv[1], 0666);
+
+  fd = open(argv[1], O_RDWR);
+  if(fd == -1){
+    printf("Error in opening the file...");
+    return 1;
+  }
+
+  srand(time(NULL)); //initialize the random number generator
+
+  //now I write 4 random numbers on the binary file
+  n=10;
+  for(int i=0; i<n; i++){
+    x=random()%20;
+    printf("%d\n",x);
+    write(fd, &x, sizeof(int));
+  }
+
+  for (i=0; i<n-1; i++) {
+   for (j=0; j<n-1-i; j++) {
+    if(fork()==0){ //siamo nel figlio
+      child(fd,j);
+      return 0;
+    }
+    else{
+	wait((int* ) 0);
+    }
+   }
+  }
+
+  lseek(fd,0,SEEK_SET); 
+  printf("\n");
+  while(read(fd,&x,sizeof(int)) == sizeof(int)){
+    printf("%d\n",x);
+  }
+  close(fd);
+  return 0;
+}
+
+void child (int fd,int j){
+  int v1, v2, c1, c2;
+
+  lseek (fd, j*sizeof (int), SEEK_SET);
+  c1 = read (fd, &v1, sizeof (int));
+  //lseek (fd, (j+1)*sizeof (int), SEEK_SET);
+  c2 = read (fd, &v2, sizeof (int));
+  if (c1<=0 || c2<=0) {
+    fprintf (stderr, "Error reading file\n");
+    exit (-1);
+  }
+  if (v1 > v2) {
+    lseek (fd, j*sizeof (int), SEEK_SET);
+    c1 = write (fd, &v2, sizeof (int));
+    //lseek (fd, (j+1)*sizeof (int), SEEK_SET);
+    c2 = write (fd, &v1, sizeof (int));
+  }
+
+  return;
+}
